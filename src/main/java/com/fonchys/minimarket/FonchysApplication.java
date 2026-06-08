018b19ae3219aa9fbb6e9e50343143e59401e051
GaBrie-herrera <u23251874@utp.edu.pe>
2026-06-08 00:35:56 -0500

    cambios . se agrego texto

--- src/main/java/com/fonchys/minimarket/FonchysApplication.java
+++ src/main/java/com/fonchys/minimarket/FonchysApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// v1.0.0 - Sistema de gestión Fonchys Minimarket
 public class FonchysApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FonchysApplication.class, args);
     }
